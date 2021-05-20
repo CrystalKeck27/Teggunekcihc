e6839b8c708e918924e97e153efbe5d3f006c36f
CrystalKeck27 <45602803+CrystalKeck27@users.noreply.github.com>
2021-05-19 20:48:11 -0500

    removed accidental line.

--- src/main/java/xyz/crystaline/teggunnekcihc/setup/ModItems.java
+++ src/main/java/xyz/crystaline/teggunnekcihc/setup/ModItems.java
@@ -4,7 +4,6 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraftforge.fml.RegistryObject;
 
-
 @SuppressWarnings("unused")
 public class ModItems {
     public static final RegistryObject<Item> SILVER_INGOT = Registration.ITEMS.register("silver_ingot", () ->
