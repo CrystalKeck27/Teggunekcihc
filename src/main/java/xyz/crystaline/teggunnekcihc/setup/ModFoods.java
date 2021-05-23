987f18b1f5218fd7ca3a44440849a742e66916c0
CrystalKeck27 <45602803+CrystalKeck27@users.noreply.github.com>
2021-05-23 17:13:48 -0500

    made cheez-its always edible

--- src/main/java/xyz/crystaline/teggunnekcihc/setup/ModFoods.java
+++ src/main/java/xyz/crystaline/teggunnekcihc/setup/ModFoods.java
@@ -4,5 +4,5 @@ import net.minecraft.item.Food;
 
 public class ModFoods {
     public static final Food CHICKEN_NUGGET = new Food.Builder().nutrition(1).saturationMod(0.3f).meat().fast().build();
-    public static final Food CHEEZ_IT = new Food.Builder().nutrition(0).saturationMod(0.1f).fast().build();
+    public static final Food CHEEZ_IT = new Food.Builder().nutrition(0).saturationMod(0.1f).fast().alwaysEat().build();
 }
